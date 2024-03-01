19adeed22cef5a843e72b348d94620f79ebd24a1
Mateusz Migas <matuszmigas.dev@gmail.com>
2024-03-01 17:58:55 +0100

    Added tailwindcss

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const isDesktop = "TAURI_PLATFORM" in process.env;
+
 export default defineConfig(async () => ({
   plugins: [react()],
   clearScreen: false,
   server: {
+    open: !isDesktop,
     port: 1420,
     strictPort: true,
     watch: {},
