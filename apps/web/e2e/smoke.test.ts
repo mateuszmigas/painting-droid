f3b8677f3cd76446c71f166609df30498f943e44
Mateusz Migas <54471371+mateuszmigas@users.noreply.github.com>
2024-04-05 16:03:49 +0200

    Fix e2e test

--- apps/web/e2e/smoke.test.ts
+++ apps/web/e2e/smoke.test.ts
@@ -3,7 +3,7 @@ import { test, expect } from "@playwright/test";
 test.describe("Smoke", () => {
   test("App has proper title", async ({ page }) => {
     await page.goto("/");
-    await expect(page).toHaveTitle("Painting Droid");
+    await expect(page).toHaveTitle("Painting Droid (Alpha)");
   });
 });
 
