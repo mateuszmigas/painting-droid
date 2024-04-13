a3923bb3138c7fe55e3a6bb3ae725ba97b890d6d
Mateusz Migas <54471371+mateuszmigas@users.noreply.github.com>
2024-04-13 22:16:10 +0200

    Add safe storage implementation

---
+++ apps/desktop/src/safe_storage.rs
@@ -0,0 +1,21 @@
+use keyring::{Entry, Result};
+
+pub struct SafeStorage {
+    entry: Entry,
+}
+
+impl SafeStorage {
+    pub fn new(name: &str) -> Result<SafeStorage> {
+        let service = "Painting Droid Safe Storage";
+        let entry = Entry::new(service, name)?;
+        Ok(SafeStorage { entry })
+    }
+
+    pub fn set_password(&self, password: &str) -> Result<()> {
+        self.entry.set_password(password)
+    }
+
+    pub fn get_password(&self) -> Result<String> {
+        self.entry.get_password()
+    }
+}
