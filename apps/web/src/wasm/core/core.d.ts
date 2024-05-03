9ede21eec425211d1088f0204b4289f9c7ec60b4
Mateusz Migas <54471371+mateuszmigas@users.noreply.github.com>
2024-05-03 16:54:13 +0200

    custom type from rust

--- apps/web/src/wasm/core/core.d.ts
+++ apps/web/src/wasm/core/core.d.ts
@@ -10,81 +10,6 @@ export function grayscale(pixels: Uint8Array): Uint8Array;
 * @returns {Uint8Array}
 */
 export function sepia(pixels: Uint8Array): Uint8Array;
-/**
-* @param {Uint8Array} pixels
-* @param {number} width
-* @param {number} height
-* @param {number} new_width
-* @param {number} new_height
-* @returns {Uint8Array}
-*/
-export function resize(pixels: Uint8Array, width: number, height: number, new_width: number, new_height: number): Uint8Array;
-/**
-* @param {string} s
-* @returns {string}
-*/
-export function greet(s: string): string;
-/**
-* Allowed pixel value range
-*
-* C.f. `VideoFullRangeFlag` variable specified in ISO/IEC 23091-4/ITU-T H.273
-*/
-export enum PixelRange {
-/**
-* Studio swing representation
-*/
-  Limited = 0,
-/**
-* Full swing representation
-*/
-  Full = 1,
-}
-/**
-* Sample position for subsampled chroma
-*/
-export enum ChromaSamplePosition {
-/**
-* The source video transfer function must be signaled
-* outside the AV1 bitstream.
-*/
-  Unknown = 0,
-/**
-* Horizontally co-located with (0, 0) luma sample, vertically positioned
-* in the middle between two luma samples.
-*/
-  Vertical = 1,
-/**
-* Co-located with (0, 0) luma sample.
-*/
-  Colocated = 2,
-}
-/**
-*/
-export enum Tune {
-  Psnr = 0,
-  Psychovisual = 1,
-}
-/**
-* Chroma subsampling format
-*/
-export enum ChromaSampling {
-/**
-* Both vertically and horizontally subsampled.
-*/
-  Cs420 = 0,
-/**
-* Horizontally subsampled.
-*/
-  Cs422 = 1,
-/**
-* Not subsampled.
-*/
-  Cs444 = 2,
-/**
-* Monochrome.
-*/
-  Cs400 = 3,
-}
 
 export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
 
@@ -92,12 +17,9 @@ export interface InitOutput {
   readonly memory: WebAssembly.Memory;
   readonly grayscale: (a: number, b: number, c: number) => void;
   readonly sepia: (a: number, b: number, c: number) => void;
-  readonly resize: (a: number, b: number, c: number, d: number, e: number, f: number, g: number) => void;
-  readonly greet: (a: number, b: number, c: number) => void;
   readonly __wbindgen_add_to_stack_pointer: (a: number) => number;
   readonly __wbindgen_malloc: (a: number, b: number) => number;
   readonly __wbindgen_free: (a: number, b: number, c: number) => void;
-  readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
 }
 
 export type SyncInitInput = BufferSource | WebAssembly.Module;
--- apps/web/src/wasm/core/core.worker.ts
+++ apps/web/src/wasm/core/core.worker.ts
@@ -1,29 +1,10 @@
 import { createProxyServer } from "@/utils/worker";
-import init, { resize, grayscale, sepia } from "./core";
+import init, { grayscale, sepia } from "./core";
 import coreUrl from "./core_bg.wasm?url";
 import { ImageUncompressed } from "@/utils/imageData";
-import { Size } from "@/utils/common";
 
 const coreServer = {
   init: () => Promise.resolve(),
-  resize: async (
-    imageData: ImageUncompressed,
-    newSize: Size
-  ): Promise<ImageUncompressed> => {
-    const view = new Uint8Array(imageData.data.buffer);
-    const result = resize(
-      view,
-      imageData.width,
-      imageData.height,
-      newSize.width,
-      newSize.height
-    );
-    return {
-      width: newSize.width,
-      height: newSize.height,
-      data: new Uint8ClampedArray(result.buffer),
-    };
-  },
   grayscale: async (
     imageData: ImageUncompressed
   ): Promise<ImageUncompressed> => {
