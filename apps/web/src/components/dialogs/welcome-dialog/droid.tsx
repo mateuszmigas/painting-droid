a93641a543cc9edc602fbfdb49c7b531ec654c39
Mateusz Migas <54471371+mateuszmigas@users.noreply.github.com>
2024-06-10 22:59:11 +0200

    animating logo

--- apps/web/src/components/dialogs/welcome-dialog/droid.tsx
+++ apps/web/src/components/dialogs/welcome-dialog/droid.tsx
@@ -1,25 +1,41 @@
 export const Droid = () => {
-  const fill = "black";
+  const fill = "blue";
   const stroke = "white";
   const strokeWidth = 2;
-
+  const duration = "1s";
   return (
-    <div className="w-96 h-96 bg-blue-900">
-      <svg height={"300"} width={"200"} className="bg-red-900">
+    <div className="w-96_ h-96_">
+      <svg height={"300"} width={"200"} className="border">
         {/* head */}
         <path
-          transform="translate(50, 50)"
-          d="M 0 50 A 50 50 0 0 1 100 50 L 0 50 Z"
+          d="M 50 100 A 50 50 0 0 1 150 100 L 50 100 Z"
           fill={fill}
           stroke={stroke}
           strokeWidth={strokeWidth}
-        />
+        >
+          <animateTransform
+            attributeName="transform"
+            type="translate"
+            from="0,160"
+            to="0,0"
+            dur={duration}
+            fill="freeze"
+          />
+          <animateTransform
+            attributeName="transform"
+            type="rotate"
+            values="0 50 50; 360 50 50"
+            keyTimes="0; 1"
+            dur="2s"
+            additive="sum"
+          />
+        </path>
         {/* eyes */}
         <circle
           cx="100"
           cy="70"
           r="10"
-          fill={fill}
+          fill={"red"}
           stroke={stroke}
           strokeWidth={strokeWidth}
         />
@@ -29,7 +45,7 @@ export const Droid = () => {
           y="90"
           width="40"
           height="10"
-          fill={fill}
+          fill={"black"}
           stroke={stroke}
           strokeWidth={strokeWidth}
         />
