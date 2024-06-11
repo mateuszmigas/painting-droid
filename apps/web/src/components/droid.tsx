12487a5e2bbd07e8d9a36b4f3a874bff181d50de
Mateusz Migas <54471371+mateuszmigas@users.noreply.github.com>
2024-06-11 20:33:22 +0200

    improve droid hands

--- apps/web/src/components/droid.tsx
+++ apps/web/src/components/droid.tsx
@@ -64,20 +64,35 @@ export const Droid = memo((props: { typingDurationSeconds: number }) => {
         x="20"
         y="120"
         width="20"
-        height="80"
+        height="60"
         rx="5"
         ry="5"
       />
+
+      <g {...commonProps} transform="translate(12, 195)" stroke-width="4">
+        <g transform="rotate(-45)">
+          <path d="m9.06 11.9 8.07-8.06a2.85 2.85 0 1 1 4.03 4.03l-8.06 8.08" />
+          <path d="M7.07 14.94c-1.66 0-3 1.35-3 3.02 0 1.33-2.5 1.52-2 2.02 1.08 1.1 2.49 2.02 4 2.02 2.2 0 4-1.8 4-4.04a3.01 3.01 0 0 0-3-3.02z" />
+        </g>
+      </g>
       {/* right arm */}
       <rect
         {...commonProps}
         x="160"
         y="120"
         width="20"
-        height="80"
+        height="60"
         rx="5"
         ry="5"
       />
+      <g {...commonProps} transform="translate(182, 205)">
+        <g transform="rotate(180)">
+          <path d="M10 2v2" />
+          <path d="M14 2v4" />
+          <path d="M17 2a1 1 0 0 1 1 1v9H6V3a1 1 0 0 1 1-1z" />
+          <path d="M6 12a1 1 0 0 0-1 1v1a2 2 0 0 0 2 2h2a1 1 0 0 1 1 1v2.9a2 2 0 1 0 4 0V17a1 1 0 0 1 1-1h2a2 2 0 0 0 2-2v-1a1 1 0 0 0-1-1" />
+        </g>
+      </g>
       {/* left leg */}
       <rect
         {...commonProps}
@@ -101,4 +116,3 @@ export const Droid = memo((props: { typingDurationSeconds: number }) => {
     </svg>
   );
 });
-
