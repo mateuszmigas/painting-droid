9c5be203c9ae7121c9672e5316a5070c651552a6
Mateusz Migas <54471371+mateuszmigas@users.noreply.github.com>
2024-07-05 20:01:27 +0200

    fix test

--- apps/web/src/features.ts
+++ apps/web/src/features.ts
@@ -2,7 +2,9 @@ import { isMobile, platform } from "./utils/platform";
 
 export const features = {
   offscreenCanvas:
-    platform !== "e2e" && "oncontextlost" in new OffscreenCanvas(0, 0),
+    platform !== "e2e" &&
+    typeof OffscreenCanvas !== "undefined" &&
+    "oncontextlost" in new OffscreenCanvas(0, 0),
   nativeMenuBar: false, //waiting for tauri fix, platform === "darwin",
   nativeColorPicker: isMobile(),
   shareFiles: "share" in navigator,
